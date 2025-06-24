ef414652a29daafea5f94284ce02be9311baed9d
Ryan WITTERT <ryan.wittert@outlook.fr>
2025-06-24 16:33:22 +0200

    debut setup firebase

---
+++ components/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Configuration de base pour Firebase
+import { initializeApp } from '@react-native-firebase/app';
+import { getFirestore } from '@react-native-firebase/firestore';
+import analytics from '@react-native-firebase/analytics';
+import Config from 'react-native-config';
+
+// Configuration Firebase
+const firebaseConfig = {
+  apiKey: Config.API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  projectId: Config.PROJECT_ID,
+  storageBucket: Config.STORAGE_BUCKET,
+  messagingSenderId: Config.MESSAGING_SENDER_ID,
+  appId: Config.APP_ID,
+  measurementId: Config.MEASUREMENT_ID,
+};
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Obtenir l'instance de Firestore
+const firestore = getFirestore(app);
+
+// Déjà setup si besoin d'utiliser les données de la bdd
+//const analyticsInstance = analytics();
